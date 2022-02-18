2d0e36aeec7624b120c0088ba22e992ad22c6bee
PetxCode <brighterdayscodelab@gmail.com>
2022-02-18 21:22:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const port = process.env.PORT || 9009;
+
+const url = "mongodb://localhost/election";
+
+const url_online =
+  "mongodb+srv://AuthClass:AuthClass@codelab.u4drr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const app = express();
+
+mongoose.connect(url_online).then(() => {
+  console.log("database connected");
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/", async (req, res) => {
+  res.end("Welcome to Our voting API server");
+});
+
+app.use("/api", require("./router"));
+
+app.listen(port, () => {
+  console.log("server is connected");
+});
